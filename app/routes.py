00caabe81345a5d822c79d2167166a6e0e326f9e
Piyusha Awate <piyushaa424@gmail.com>
2026-02-04 22:06:00 +0530

    completed login feature

--- app/routes.py
+++ app/routes.py
@@ -1,6 +1,12 @@
+from flask import render_template
 from app import app
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return "Hello, World!"
+    user = {'username': 'Miguel'}
+    posts = [
+        {'author': {'username': 'John'}, 'body': 'Beautiful day!'},
+        {'author': {'username': 'Susan'}, 'body': 'Flask is cool!'}
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
